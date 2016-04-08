77e0110a03b7cef6c285974c823550388ca512e1
Ben Sayeg <bensayeg@gmail.com>
2016-04-08 13:32:22 -0400

    Update mdhtmlform.js

--- examples/simple/js/mdhtmlform.js
+++ examples/simple/js/mdhtmlform.js
@@ -12,8 +12,8 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+ $("button#preview-html").on("click", function(e) {
 
-(function() {
   var MdHtmlForm;
 
   MdHtmlForm = (function() {
@@ -102,9 +102,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
   })();
 
-      $(".mdhtmlform-html").hide();
+     
 
-       $("button#preview-html").on("click", function(e) {
+      
          $(function() {
              return $(".mdhtmlform-md").each(function() {
                  return new MdHtmlForm(this);
@@ -122,4 +122,4 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
   
 
-}).call(this);
+});
